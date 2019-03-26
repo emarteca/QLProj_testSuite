05ea0eed79a8101e8b5efd1ad084ecd42dcb9fc6
Ellen Arteca <ellen.arteca@gmail.com>
2019-03-26 12:11:10 -0400

    one more test, this one with a \"this\" awaited explicitly. also fixed a typo in test 7

--- ap8_test.js
+++ ap8_test.js
@@ -1,4 +1,4 @@
-// more global var stuff, longer chain
+// await directly on a this access
 
 class A {
 	constructor( b) {
@@ -27,6 +27,12 @@ class B {
 	getB() {
 		return this.theA.getB();
 	}
+
+	async waitB( newB) {
+		console.log( this.theA);
+		await this.theA.modB( newB);
+		console.log( this.theA);
+	}
 };
 
 async function apt_test_fn() {
@@ -34,10 +40,6 @@ async function apt_test_fn() {
 	var x = new B( new A(1));
 	
 	console.log( x);
-	await x.modB( 2); // this shouldnt be able to swap with the prints, since global var x is modified in the function
-	console.log( x);
-
-	console.log( x);
-	var y = await x.getB(); // here we can swap, since no global var is modified in the function
+	await x.waitB( 2); // this shouldnt be able to swap with the prints, since x is modified in the function
 	console.log( x);
 }
